a951c8433417dfe03ed979529efce75939e2da98
MarkTerry3 <mark.terry93@gmail.com>
2022-02-24 11:17:04 -0600

    some reducer stuff done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App/App';
+import App from './components/App/App';/////////////
+import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
+
+
+const pizzaChoice = (state = [], action) => {
+    if (action.type === '') {
+        return action.payload;
+    }
+    return state;
+}
+
+// The store is the big JavaScript Object that holds all of the information for our application
+const reduxStore = createStore(
+    combineReducers({
+        pizzaChoice,
+    }),
+    applyMiddleware(logger),
+);
+
+
+
+
+
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={reduxStore}>
+    <App />
+</Provider>, 
+document.getElementById('root'));
