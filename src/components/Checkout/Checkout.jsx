4a13eec934afea99a64376fc4b6ff05d18e6e1c3
MarkTerry3 <mark.terry93@gmail.com>
2022-02-24 13:39:03 -0600

    Checkout Component Made

---
+++ src/components/Checkout/Checkout.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ProductList from '../ProductList/ProductList';
+import {useSelector} from 'react-redux';
+import {useDispatch} from 'react-redux';
+import "./Checkout.css";
+
+
+
+function Checkout() {
+
+
+
+
+
+    return(
+        <>
+        <header className='App-header'></header>
+
+        </>
+    )
+}// end Checkout
+
+
+export default Checkout;
