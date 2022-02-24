92119193795925e045e7cbaa8388a350b98d83d4
MarkTerry3 <mark.terry93@gmail.com>
2022-02-24 14:02:13 -0600

    Checkout page and component made

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -9,12 +9,32 @@ import "./Checkout.css";
 function Checkout() {
 
 
+    const handleCheckout = () => {
+
+
+    // POST or GET route in a function
+
+
+    }
+
 
 
 
     return(
         <>
-        <header className='App-header'></header>
+        <header className='App-header'>CheckOut</header>
+        <div>
+            <h3>Step 3: Checkout</h3>
+        </div>
+
+        <div>
+            //customer Info goes here
+        </div>
+
+        
+
+        <button onClick={handleCheckout}>Checkout</button>
+
 
         </>
     )
