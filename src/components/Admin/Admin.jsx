9a19368072867720e637e00e4a003d4157973342
Adam Garberg <adamgarberg@gmail.com>
2022-02-24 14:20:40 -0600

    admin started

---
+++ src/components/Admin/Admin.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+
+
+
+function Admin(){
+    const dispatch = useDispatch()
+
+    
+    axios({
+        method: 'GET',
+        url: '/api/order'
+      }).then( response => {
+        // response.data is array of orders
+        console.log(response.data);
+        dispatch({
+          type: 'SET_PIZZA_ORDERS',
+          payload: response.data
+
+        })
+      }).catch( error => {
+        console.log('error on GET', error);
+      });
+    }
+
+
+
+
+
+    return(
+
+
+
+
+    )
+
+}
+
+
+
+
+
+
+export default Admin
